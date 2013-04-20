e8870104b37516f5f57a7569ab57956181cd10a9
Chris Greene <archgoon@gmail.com>
2013-04-20 14:48:56 -0700

    Added additional logic to setup script.
    It's now safer to execute on a machine that's already setup.

--- first_time.sh
+++ first_time.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
-mkdir ~/bin
-mkdir ~/src
-mkdir ~/.shell_setup
-cp * ~/.shell_setup
-cp bin/* ~/bin
-echo "
+if [ ! -e first_time.sh ]; then
+    echo "execute from same directory as 'first_time.sh'"
+    exit 1
+fi
+if [ ! -e ~/bin ]; then
+    echo Making ~/bin
+    mkdir ~/bin
+fi
+if [ ! -e ~/bin ]; then
+    echo Making ~/src
+    mkdir ~/src
+fi
+echo Copying files to bin directory
+for file in bin/*; do
+    if [ ! -e ~/$file ]; then
+        echo Copying $file to bin
+        cp $file ~/bin
+    else
+        echo ~/$file exists 
+    fi
+done
+if [ -z "$(grep '\.shell_setup' ~/.bashrc 2> /dev/null)" ]; then
+    echo "Adding initialization to .bashrc"
+    echo "
 cd ~/.shell_setup
 source bashrc
-cd ~/
-" >> ~/.bashrc
+cd ~/" >> ~/.bashrc
+fi
