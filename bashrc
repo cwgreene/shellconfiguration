1a35b7f7fc8b2819edb336433b4a2cf83fa302b8
Chris Greene <archgoon@gmail.com>
2013-07-13 12:02:42 -0700

    Added more completions logic.

--- bashrc
+++ bashrc
@@ -2,3 +2,5 @@ source colors
 source functions
 source prompt
 source paths
+source variables
+addpath ~/.shell_setup/bin
--- bin/addpath.py
+++ bin/addpath.py
@@ -2,12 +2,21 @@
 import sys
 import os
 
-def add_path(path, new_path):
+def add_path(path, new_path, pos=None):
     paths = path.split(":")
     if new_path in paths:
         return path
-    paths.append(new_path)
+    if pos == None:
+        pos = len(paths)
+    paths.insert(pos,new_path)
     return ":".join(paths)
+
 def main():
-    print add_path(os.environ["PATH"], sys.argv[1])
+    path = ""
+    if len(sys.argv) >= 2:
+        path = os.path.abspath(sys.argv[1])
+    if len(sys.argv) == 2:
+        print add_path(os.environ["PATH"], sys.argv[1])
+    elif len(sys.argv) == 3:
+        print add_path(os.environ["PATH"], sys.argv[1], int(sys.argv[2]))
 main()
