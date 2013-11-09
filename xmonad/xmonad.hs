9820add80fd5bf6d87aa94401e5f73800f9ecdb2
Chris Greene <archgoon@gmail.com>
2013-11-09 12:33:59 -0800

    Nicer xmonad config.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,5 +1,27 @@
 import XMonad
+import XMonad.Layout.NoBorders
+import XMonad.Hooks.ICCCMFocus
+import XMonad.Hooks.DynamicLog
 
 main = do
-    xmonad $ defaultConfig
-        {borderWidth = 3}
+    xmonad =<< xmobar defaultConfig
+        {borderWidth = 3,
+         focusedBorderColor = "#ff000",
+         logHook = takeTopFocus,
+         layoutHook = myLayout,
+         terminal="gnome-terminal"}
+
+myLayout = tiled ||| Mirror tiled ||| noBorders Full
+    where
+     -- default tiling algorithm partitions the screen into two panes
+     tiled   = Tall nmaster delta ratio
+
+     -- The default number of windows in the master pane
+     nmaster = 1
+
+     -- Default proportion of screen occupied by master pane
+     ratio   = 1/2
+
+     -- Percent of screen to increment by when resizing panes
+     delta   = 3/100
+
