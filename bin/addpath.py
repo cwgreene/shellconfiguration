d1826efbfa84caa8b97adbafa4fa75460d24d86e
Chris Greene <archgoon@gmail.com>
2020-10-01 11:01:01 -0700

    Add support for pathfiles.

--- bin/addpath.py
+++ bin/addpath.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import sys
 import os
+import argparse
 
 def add_path(path, new_path, pos=None):
     paths = path.split(":")
@@ -9,14 +10,35 @@ def add_path(path, new_path, pos=None):
     if pos == None:
         pos = len(paths)
     paths.insert(pos,new_path)
+    afile = open("/tmp/add_path.log", "a")
+    print>>afile, path, new_path
     return ":".join(paths)
 
+def add_paths(path, pathfile):
+    paths = path.split(":")
+    paths_set = set(paths)
+    new_paths = []
+    with open(pathfile) as new_paths_file:
+        for line in new_paths_file.read().split("\n"):
+            line = os.path.expanduser(line)
+            if line in paths_set:
+                continue
+            if line.strip() == "":
+                continue
+            new_paths.append(line)
+    return ":".join(new_paths + paths)
+
 def main():
-    path = ""
-    if len(sys.argv) >= 2:
-        path = os.path.abspath(sys.argv[1])
-    if len(sys.argv) == 2:
-        print add_path(os.environ["PATH"], sys.argv[1])
-    elif len(sys.argv) == 3:
-        print add_path(os.environ["PATH"], sys.argv[1], int(sys.argv[2]))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--pathfile")
+    parser.add_argument("--path", default=None)
+    parser.add_argument("--at", type=int, default=0)
+    options = parser.parse_args()
+    if not options.path and not options.pathfile:
+        parser.print_help()
+        return
+    if options.pathfile:
+        print(add_paths(os.environ["PATH"], options.pathfile))
+    else:
+        print add_path(os.environ["PATH"], options.path, options.at)
 main()
