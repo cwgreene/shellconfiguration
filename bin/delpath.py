d1a22272d94f979345e5bb176e3dd9f364d58d86
Chris Greene <archgoon@gmail.com>
2022-08-11 22:28:53 -0700

    Update delpath.py
    migrate to python3

--- bin/delpath.py
+++ bin/delpath.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import sys
 import os
 
@@ -9,5 +9,5 @@ def del_path(path, new_path):
     paths.remove(new_path)
     return ":".join(paths)
 def main():
-    print del_path(os.environ["PATH"], sys.argv[1])
+    print(del_path(os.environ["PATH"], sys.argv[1]))
 main()
